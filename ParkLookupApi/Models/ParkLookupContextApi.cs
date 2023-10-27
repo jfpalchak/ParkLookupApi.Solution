d43b078c565caf2e0e6e35ef75de1e41250d8b7f
Joey Palchak <joey.palchak@gmail.com>
2023-10-27 13:41:45 -0700

    edit formatting for readability

--- ParkLookupApi/Models/ParkLookupContextApi.cs
+++ ParkLookupApi/Models/ParkLookupContextApi.cs
@@ -16,9 +16,30 @@ public class ParkLookupApiContext : IdentityDbContext<ApplicationUser>
 
     builder.Entity<Park>()
       .HasData(
-        new Park { ParkId = 1, Name = "Acadia National Park", Location = "Maine", Description = "Today, Acadia preserves about 40,000 acres of Atlantic coast shoreline, mixed hardwood and spruce/fir forest, mountains, and lakes, as well as several offshore islands.", Category = "National Park" },
-        new Park { ParkId = 2, Name = "Badlands National Park", Location = "South Dakota", Description = "Prairie grasslands support bison, bighorn sheep, deer, pronghorn antelope, swift fox, and black-footed ferrets.", Category = "National Park" },
-        new Park { ParkId = 3, Name = "Cape Kiwanda", Location = "Oregon", Description = "This sandstone headland just north of Pacific City offers one of the best viewpoints on the coast for witnessing the ocean's power. The landmark is one of three along the Three Capes Scenic Route (along with Cape Meares and Cape Lookout).", Category = "State Park" }
+        new Park 
+        { 
+          ParkId = 1, 
+          Name = "Acadia National Park", 
+          Location = "Maine", 
+          Description = "Today, Acadia preserves about 40,000 acres of Atlantic coast shoreline, mixed hardwood and spruce/fir forest, mountains, and lakes, as well as several offshore islands.", 
+          Category = "National Park" 
+        },
+        new Park 
+        { 
+          ParkId = 2, 
+          Name = "Badlands National Park", 
+          Location = "South Dakota", 
+          Description = "Prairie grasslands support bison, bighorn sheep, deer, pronghorn antelope, swift fox, and black-footed ferrets.", 
+          Category = "National Park" 
+        },
+        new Park 
+        { 
+          ParkId = 3, 
+          Name = "Cape Kiwanda", 
+          Location = "Oregon", 
+          Description = "This sandstone headland just north of Pacific City offers one of the best viewpoints on the coast for witnessing the ocean's power. The landmark is one of three along the Three Capes Scenic Route (along with Cape Meares and Cape Lookout).", 
+          Category = "State Park" 
+        }
       );
   }
 }
