dd388365dbaee473673100a076cf2e5ab12f0370
Joey Palchak <joey.palchak@gmail.com>
2023-10-27 13:41:24 -0700

    add validation annotations

--- ParkLookupApi/Models/SignInDto.cs
+++ ParkLookupApi/Models/SignInDto.cs
@@ -3,6 +3,9 @@ namespace ParkLookupApi.Models;
 
 public class SignInDto
 {
+  [Required]
+  [EmailAddress]
   public string Email { get; set; }
+  [Required]
   public string Password { get; set; }
 }
