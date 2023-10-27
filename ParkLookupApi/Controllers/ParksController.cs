217f2cdf45bd58003a883f926726f1fd20d021a7
Joey Palchak <joey.palchak@gmail.com>
2023-10-27 09:03:05 -0700

    add GET endpoints to ParksController

--- ParkLookupApi/Controllers/ParksController.cs
+++ ParkLookupApi/Controllers/ParksController.cs
@@ -19,5 +19,27 @@ public class ParksController : ControllerBase
     _userManager = userManager;
   }
 
+  // GET: api/parks/
+  [HttpGet]
+  public async Task<ActionResult<IEnumerable<Park>>> Get()
+  {
+    IQueryable<Park> query = _db.Parks.AsQueryable();
+
+    return await query.ToListAsync();
+  }
+
+  // GET: api/parks/{id}
+  [HttpGet("{id}")]
+  public async Task<ActionResult<Park>> GetPark(int id)
+  {
+    Park thisPark = await _db.Parks.FindAsync(id);
+
+    if (thisPark == null)
+    {
+      return NotFound();
+    }
+
+    return thisPark;
+  }
 
 }
